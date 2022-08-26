7af2429069d6ad27c403604f61bb5dfafe470265
Jay Liyanapathirana <jay.liyanapathirana@learningworks.co.nz>
2022-08-26 14:34:58 +1200

    Task 2

--- index.php
+++ index.php
@@ -4,5 +4,17 @@
 	function repeat($arr){
 		return json_encode(array_merge($arr,$arr,$arr));
 	}
+	
+	//--------- Task 2 -----------------
+	function reformat($str){
+		$str = preg_replace('/[aeiou]/i', '', $str); // Remove vowels
+		$firstLetter = strtoupper(substr($str,0,1)); // First letter to uppercase
+		$lowerString = strtolower(substr($str,1)); // Rest of the string to lowercase
+		return $firstLetter.$lowerString; // Concatenate first letter and rest of the string
+	}
+
 	echo repeat([1,2,3]);
+	echo "<br>";
+	echo reformat("liMeSHArp DeveLoper TEST");
+
 ?>
